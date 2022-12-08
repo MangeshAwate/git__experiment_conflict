c68cf174d9800b0bf9517bad192a56a9c4c58872
prashant_rathour <prathour884@gmail.com>
2022-12-08 19:53:44 +0530

    i have fixed as able

--- prime_c.js
+++ prime_c.js
@@ -1,8 +1,15 @@
-uvabdivcabud
-bag=oadncod;
-{}
-8888888748r
-88888888888
-*********
-)0cdsoichndc
-vljsfjdnvhi;ubsev
+   function prime(number){
+       let counter=0;
+       for(let i=1;i<=number;i++){
+           if(number%i==0){
+               counter++;
+           }
+       }
+       if(counter==2){
+           return (number,"is prime");
+       }else{
+           return (number,"not a prime");
+       }
+   }
+let number=13;
+console.log(prime(number));//output
